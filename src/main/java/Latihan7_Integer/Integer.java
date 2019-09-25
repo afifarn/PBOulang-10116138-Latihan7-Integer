182216edb4f10c64c8348f410d473b1e793b5d13
afifarn <afifr0837@gmail.com>
2019-09-25 15:42:52 +0700

    first commit

---
+++ src/main/java/Latihan7_Integer/Integer.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan7_Integer;
+
+/**
+ *
+ * @author 
+ * @author 
+ * NAMA     : Afif Raditya Nugraha
+ * KELAS    : PBO_ULANG/IF4
+ * NIM      : 10116138
+ * LOG      : Program ini berisi program untuk tipe data Integer
+ */
+public class Integer {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        byte b; 
+        short s;
+        int i;
+        long l;
+        
+        b = 120;
+        s = 32767;
+        i = 1000_000_000;
+        l = 9000000000L;
+        
+        System.out.println("Byte : " + b);
+        System.out.println("Short : " + s);
+        System.out.println("Int : " + i);
+        System.out.println("Long : " + l);
+    }
+    
+}
